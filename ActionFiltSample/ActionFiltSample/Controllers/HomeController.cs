f65f56d82a1910b7c6d113eb5ec07331bd897118
uday.bavanasi123 <uday.bavanasi123@gmail.com>
2019-10-19 14:42:03 -0700

    MVC Application

---
+++ ActionFiltSample/ActionFiltSample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace ActionFiltSample.Controllers
+{
+    public class HomeController : Controller, IActionFilter
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Trace.WriteLine("Calling View from Controller" + DateTime.Now.ToString());
+            return View();
+        }
+
+        void IActionFilter.OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //post processing logic
+            Trace.WriteLine("Action has executed Post" + DateTime.Now.ToString());
+
+        }
+        void IActionFilter.OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //preprocessing logic
+            Trace.WriteLine("Action is executing Pre" + DateTime.Now.ToString());
+        }
+    }
+}
---
+++ ActionFiltSample/ActionFiltSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ActionFiltSample.MvcApplication" Language="C#" %>
